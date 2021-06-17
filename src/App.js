28e9c5ae887fbc5ff01fa5d312d76d4cbac95b65
Liam Hart <liamhart700@gmail.com>
2021-06-17 18:08:22 +0930

    Add dynamic date header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 
 function App() {
+  const date = new Date();
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ date.toDateString() }</h1>
         </div>
     </div>
   );
