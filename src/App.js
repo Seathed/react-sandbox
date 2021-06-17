b11f8ba866479ff4144ce6c498a80123b36a8155
Liam Hart <liamhart700@gmail.com>
2021-06-17 18:16:27 +0930

    Add Navbar, Home and Calendar Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home'
 
 function App() {
   const date = new Date();
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
         <h1>{ date.toDateString() }</h1>
+        <Home />
         </div>
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Calendar</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="#">Calendar Settings</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
