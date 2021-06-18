b5534b4b2f707be426fc8cd9736043dd585bd92f
Liam Hart <liamhart700@gmail.com>
2021-06-18 17:45:13 +0930

    dynamically fetch events from a json-server

--- src/Home.js
+++ src/Home.js
@@ -4,13 +4,7 @@ import EventWrapper from './EventWrapper';
 
 const Home = () => {
 
-    const [events, setEvents] = useState([
-        {title: "First Event", body: "lorem ipsum...", host: "Abby", id: 1},
-        {title: "Second Event", body: "lorem ipsum...", host: "Liam", id: 2},
-        {title: "Third Event", body: "lorem ipsum...", host: "James", id: 3},
-        {title: "Fourth Event", body: "lorem ipsum...", host: "Cheyenne", id: 4},
-        {title: "Fifth Event", body: "lorem ipsum...", host: "Liam", id: 5},
-    ]);
+    const [events, setEvents] = useState(null);
 
     const handleDeleteEvent = (id) => {
         setEvents(events.filter(e => e.id !== id));
@@ -19,16 +13,21 @@ const Home = () => {
     const [pageStateText, setPageStateText] = useState("Hello, this is the default page state.");
 
     // [] only runs on initial page render
+    // [events] runs on initial page render and anytime the events variable changes
     useEffect(() => {
-        console.log('use effect ran');
-    }, [events]);
+        fetch('http://localhost:8000/events').then((res) => {
+            return res.json();
+        }).then((data) => {
+            setEvents(data);
+        });
+    }, []);
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
             <p>{ pageStateText }</p>
-            <EventWrapper events={events} title="All Events" handleDelete={ handleDeleteEvent }/>
-            <EventWrapper events={events.filter((e) => e.host === 'Liam')} title="Liam's Events" />
+            {events && <EventWrapper events={events} title="All Events" handleDelete={ handleDeleteEvent }/>}
+            {events && <EventWrapper events={events.filter((e) => e.host === 'Liam')} title="Liam's Events" handleDelete={ handleDeleteEvent }/>}
         </div>
     );
 }
