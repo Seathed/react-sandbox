44bbd2dce61d50f5807adddd41a5db2e1c636432
Liam Hart <liamhart700@gmail.com>
2021-06-18 16:45:13 +0930

    import useState from react library and use it to create dynamic web page content

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,22 @@
+import { useState } from 'react';
+
 const Home = () => {
+
+    const [pageStateText, setPageStateText] = useState("Hello, this is the default page state.");
+
     const handleClick = () => {
         console.log("Button clicked...");
     }
-    const handleArgumentButtonClick = (name) => {
-        console.log("Hello, " + name);
+    const handleClick2 = () => {
+        setPageStateText("Hello, this is the modified page state.");
     }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            <button onClick={handleClick}>Button</button>
-            <button onClick={() => {handleArgumentButtonClick('Larry')}}>Button with Arguments</button>
+            <p>{ pageStateText }</p>
+            <button onClick={ handleClick }>Button</button>
+            <button onClick={ handleClick2 }>Change Page State</button>
         </div>
     );
 }
