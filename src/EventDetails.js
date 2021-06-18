9e6c37d41f47dab919576c8c841c7e2f77d441ed
Liam Hart <liamhart700@gmail.com>
2021-06-18 21:19:37 +0930

    Create Event Details filled out with data content with css

--- src/EventDetails.js
+++ src/EventDetails.js
@@ -1,10 +1,21 @@
 import { useParams } from "react-router-dom";
+import useFetch from './useFetch';
 
 const EventDetails = () => {
     const { id } = useParams();
+    const {data:event, isLoading, error} = useFetch(`http://localhost:8000/events/${id}`);
     return (
         <div className="event-details">
-            <h2>Event Details - #{ id }</h2>
+            {isLoading && <div><p>Loading Event...</p></div>}
+            {error && <div>{ error }</div>}
+            {event && (
+                <article>
+                    <h2>{ event.title }</h2>
+                    <p>Hosted by: { event.host }</p>
+                    <div>{ event.body }</div>
+                    <button>Delete</button>
+                </article>
+            ) }
         </div>
     );
 }
