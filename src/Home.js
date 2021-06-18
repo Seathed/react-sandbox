00c60ee11e0ae8cd6284ee109e54a4eab1b58d99
Liam Hart <liamhart700@gmail.com>
2021-06-18 16:38:55 +0930

    Create onClick events and link them to buttons

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log("Button clicked...");
+    }
+    const handleArgumentButtonClick = (name) => {
+        console.log("Hello, " + name);
+    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Button</button>
+            <button onClick={() => {handleArgumentButtonClick('Larry')}}>Button with Arguments</button>
         </div>
     );
 }
