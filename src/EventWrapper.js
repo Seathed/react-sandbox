118cdec0f906daeecf9511eba350e8f29e9d5857
Liam Hart <liamhart700@gmail.com>
2021-06-18 20:08:11 +0930

    create links to event details page from event previews

--- src/EventWrapper.js
+++ src/EventWrapper.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const EventWrapper = ({events, title, handleDelete}) => {
 
     return ( 
@@ -5,8 +7,10 @@ const EventWrapper = ({events, title, handleDelete}) => {
         <h2>{ title }</h2>
         {events.map((e) => (
             <div className="event-preview" key={e.id}>
+                <Link to={`/events/${ e.id }`}>
                 <h2>{ e.title }</h2>
                 <p>Hosted by: { e.host }</p>
+                </Link>
             </div>
         ))}
     </div>
