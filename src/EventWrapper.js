d6cf7821ebb4ef9bd8f0745021cbd7b37eb6162e
Liam Hart <liamhart700@gmail.com>
2021-06-18 17:20:55 +0930

    Create delete function to delete events.

--- src/EventWrapper.js
+++ src/EventWrapper.js
@@ -1,4 +1,4 @@
-const EventWrapper = ({events, title}) => {
+const EventWrapper = ({events, title, handleDelete}) => {
 
     return ( 
         <div className="eventWrapper">
@@ -7,6 +7,7 @@ const EventWrapper = ({events, title}) => {
             <div className="event-preview" key={e.id}>
                 <h2>{ e.title }</h2>
                 <p>Hosted by: { e.host }</p>
+                <button onClick={() => handleDelete(e.id)}>Delete Event</button>
             </div>
         ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -4,12 +4,16 @@ import EventWrapper from './EventWrapper';
 const Home = () => {
 
     const [events, setEvents] = useState([
-        {title: "FirstEvent", body: "lorem ipsum...", host: "Abby", id: 1},
+        {title: "First Event", body: "lorem ipsum...", host: "Abby", id: 1},
         {title: "Second Event", body: "lorem ipsum...", host: "Liam", id: 2},
         {title: "Third Event", body: "lorem ipsum...", host: "James", id: 3},
         {title: "Fourth Event", body: "lorem ipsum...", host: "Cheyenne", id: 4},
         {title: "Fifth Event", body: "lorem ipsum...", host: "Liam", id: 5},
     ]);
+
+    const handleDeleteEvent = (id) => {
+        setEvents(events.filter(e => e.id !== id));
+    }
 
     const [pageStateText, setPageStateText] = useState("Hello, this is the default page state.");
 
@@ -17,7 +21,7 @@ const Home = () => {
         <div className="home">
             <h2>Homepage</h2>
             <p>{ pageStateText }</p>
-            <EventWrapper events={events} title="All Events" />
+            <EventWrapper events={events} title="All Events" handleDelete={ handleDeleteEvent }/>
             <EventWrapper events={events.filter((e) => e.host === 'Liam')} title="Liam's Events" />
         </div>
     );
