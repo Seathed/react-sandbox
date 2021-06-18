db3fac288e89e0ed53df6e57d1e8593575117c6b
Liam Hart <liamhart700@gmail.com>
2021-06-18 21:20:52 +0930

    Remove unecessary imports and content from the home page

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,4 @@
-import EventWrapper from './EventWrapper';
-import useFetch from './useFetch';
-import { Link } from 'react-router-dom';
-
 const Home = () => {
-    const {data:events, isLoading, error} = useFetch('http://localhost:8000/events');
-
-    // [] only runs on initial page render
-    // [events] runs on initial page render and anytime the events variable changes
 
     return ( 
         <div className="home">
