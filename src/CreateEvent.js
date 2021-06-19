7897484aff344ee1d26747df1f9b3f626841973e
Liam Hart <liamhart700@gmail.com>
2021-06-19 16:27:03 +0930

    Add some logic to the Create New Event Form

--- src/CreateEvent.js
+++ src/CreateEvent.js
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 const CreateEvent = () => {
+    const [title, setTitle] = useState('');
+    const [startTime, setStartTime] = useState('');
+    const [endTime, setEndTime] = useState('');
+    const [host, setHost] = useState('');
+
     return ( 
         <div className="create-event">
             <h2>Create a New Event</h2>
@@ -7,20 +14,28 @@ const CreateEvent = () => {
                 <input 
                 type="text"
                 required
+                value={ title }
+                onChange={(e) => {setTitle(e.target.value)}}
                 />
                 <label>Start Time:</label>
                 <input 
                 type="datetime-local"
                 required
+                value={ startTime }
+                onChange={(e) => {setStartTime(e.target.value)}}
                 />
                 <label>End Time:</label>
                 <input 
                 type="datetime-local"
                 required
+                value={ endTime }
+                onChange={(e) => {setEndTime(e.target.value)}}
                 />
                 <label>Host:</label>
                 <input 
                 type="text"
+                value={ host }
+                onChange={(e) => {setHost(e.target.value)}}
                 />
                 <button>Create Event</button>
             </form>
