b03a2f25104eb0d2494bf3010e0acf4a789336ce
Liam Hart <liamhart700@gmail.com>
2021-06-19 16:53:13 +0930

    Add delete functionality to the delete button on Event Details page

--- src/EventDetails.js
+++ src/EventDetails.js
@@ -1,9 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import useFetch from './useFetch';
 
 const EventDetails = () => {
     const { id } = useParams();
     const {data:event, isLoading, error} = useFetch(`http://localhost:8000/events/${id}`);
+    const history = useHistory();
+
+    const handleDelete = () => {
+        fetch(`http://localhost:8000/events/${ id }`, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/events');
+        })
+    }
+
     return (
         <div className="event-details">
             {isLoading && <div><p>Loading Event...</p></div>}
@@ -13,7 +23,7 @@ const EventDetails = () => {
                     <h2>{ event.title }</h2>
                     <p>Hosted by: { event.host }</p>
                     <div>{ event.body }</div>
-                    <button>Delete</button>
+                    <button onClick={ handleDelete }>Delete</button>
                 </article>
             ) }
         </div>
